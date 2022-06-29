f4e6f21f6343a31b7da05f4e87b30dfb9996fc27
Miguel <64989931+miguelzph@users.noreply.github.com>
2022-06-29 03:49:12 +0000

    update test cadastros

--- tests/test_cadastro_funcionario.py
+++ tests/test_cadastro_funcionario.py
@@ -5,21 +5,22 @@ from src.entities.funcionario import Funcionario
 from src.business.cadastro_funcionario import CadastroFuncionario
 
 from src.exceptions.funcionario_not_found_error import FuncionarioNotFoundError
-from mysql.connector import IntegrityError
+from src.exceptions.null_data_error import NullDataError
+from src.exceptions.duplicate_entry_error import DuplicateEntryError
 
 # Algumas vezes os testes dão um erro de conexão, tentei passar o cadastro no fixture e como global e nenhum dos dois adiantou
 pytest.cadastro_funcionario = CadastroFuncionario()
 
-#@pytest.fixture()
-#def cadastro_funcionario():
+# @pytest.fixture()
+# def cadastro_funcionario():
 #    cadastro = CadastroFuncionario()
 #    return cadastro
 
 # CONSULTAR
 def test_consultar():
-    resultado = pytest.cadastro_funcionario.consultar_por_matricula(1)
+    resultado = pytest.cadastro_funcionario.consultar_por_matricula(2104)
 
-    assert resultado == {'matricula': 1, 'nome': 'Vitor', 'cpf': '01234567891', 'data_admissao': datetime.date(2022, 6, 23), 'cargo': 6, 'comissao': 'sim'}
+    assert resultado == {'matricula': 2104, 'nome': 'Ana Maria Silva', 'cpf': '11111111100', 'data_admissao': datetime.date(2019, 7, 2), 'cargo': 32, 'comissao': 'sim'}
 
 def test_consultar_nao_existente():
     
@@ -34,21 +35,34 @@ def test_listar_todos():
 
 # INCLUIR
 def test_incluir_cpf_duplicado():
-    funcionario = Funcionario('Teste_cpf_duplicado', '01234567891', '2021-10-20', 20, 1)
+    funcionario = Funcionario('Teste_cpf_duplicado', '11111111100', '2021-10-20', 20, 1)
     
-    with pytest.raises(IntegrityError):
+    with pytest.raises(DuplicateEntryError):
+        pytest.cadastro_funcionario.incluir(funcionario)
+
+def test_incluir_dado_nulo():
+    funcionario = Funcionario(None, '11111111111', '2021-10-20', 20, 1)
+    
+    with pytest.raises(NullDataError):
+        pytest.cadastro_funcionario.incluir(funcionario)
+
+def test_incluir_dado_vazio():
+    funcionario = Funcionario('Teste', '', '2021-10-20', 20, 1)
+    
+    with pytest.raises(NullDataError):
         pytest.cadastro_funcionario.incluir(funcionario)
 
 # Só consegui pensar nos 2 proximos testes sendo dependentes (para não deixar sujeira no banco)
 def test_incluir_funcionario():
-    funcionario_teste = Funcionario('testezz', '12345631111', '2021-10-20', 20, 1)
+    funcionario_teste = Funcionario('testezz', '9345631111', '2021-10-20', 20, 1)
     
     assert pytest.cadastro_funcionario.incluir(funcionario_teste) == None
 
 
-def test_excluir_funcionario(): #nao sei como pegar a matricula do anterior
-    # o ideal seria excluir o que foi adicionado no anterior
-    assert pytest.cadastro_funcionario.excluir_por_matricula(2084) == True
+def test_excluir_funcionario(): 
+  matricula_teste = pytest.cadastro_funcionario.listar_todos()[-1]['matricula']
+  assert pytest.cadastro_funcionario.excluir_por_matricula(matricula_teste) == True
+
     
 
 def test_excluir_nao_existente():
