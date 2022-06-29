7ba2725270ffbfbe5f4684c7d1c04d6c1cf54554
Miguel <64989931+miguelzph@users.noreply.github.com>
2022-06-29 15:41:15 +0000

    testando menu funcionarios

--- sistema.py
+++ sistema.py
@@ -2,11 +2,15 @@ from src.database.db import DB
 from src.business.cadastro_funcionario import CadastroFuncionario
 from src.business.holerite import Holerite
 from src.entities.funcionario import Funcionario
+import os
+
 
 
 def menu_funcionario(cadastro):
     while True:
-        option = input('''    -----MENU-----
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+        option = input('''    -----MENU FUNCIONARIOS-----
     [1]: Adicionar funcionário
     [2]: Consultar funcionário
     [3]: Listar todos os funcionários
@@ -22,11 +26,11 @@ def menu_funcionario(cadastro):
     
         if option == '1':
             try:
-                nome = input('Digite o nome:')
-                cpf = input('Digite o nome:')
-                data_admissao = input('Digite o nome:')
-                cargo = int(input('Digite o codigo do cargo:'))
-                comissao = input('O Funcionaio possui comissão:')
+                nome = input('Digite o nome: ')
+                cpf = input('Digite o cpf [apenas dígitos]: ')
+                data_admissao = input('Digite a data [ano-mes-dia]: ')
+                cargo = input('Digite o código do cargo: ')
+                comissao = input('O Funcionaio possui comissão [sim; nao]? ')
                 funcionario = Funcionario(nome, cpf, data_admissao, cargo, comissao)
                 cadastro.incluir(funcionario, commit=True)
             except Exception as ex:
@@ -48,8 +52,8 @@ def menu_funcionario(cadastro):
         elif option == '4':
             try:
                 matricula = int(input('Digite a matricula: '))
-                campo = input('Qual campo será alterado? ')
-                novo_valor = input(f'Qual o novo valor do campo {campo}')
+                campo = input('Qual campo será alterado?[nome, cpf, data_admissao, comissao, cargos_codigo]')
+                novo_valor = input(f'Qual o novo valor do campo {campo}?')
                 cadastro.alterar_por_matricula(matricula, campo, novo_valor)
                 print('A informação foi alterada!')
             except Exception as ex:
@@ -58,10 +62,10 @@ def menu_funcionario(cadastro):
         elif option == '5':
             try:
                 matricula = int(input('Digite a matricula: '))
-                cadastro.excluir_por_matricula(matricula)
+                cadastro.excluir_por_matricula(matricula, commit=True)
                 print(f'Funcionário de matricula {matricula} excluido!')
             except Exception as ex:
-                print(f'Entrada invalida! {ex}')
+                print(f'Entrada invalida: {ex}')
                 
         elif option == '9':
             return None
@@ -72,25 +76,29 @@ def menu_funcionario(cadastro):
             print('Opção invalida!')
         
         print('\n')
-        input('Aperte ENTER para exibir o menu.')
+        input('Aperte ENTER para voltar ao menu.')
         print('\n')
         
 
 
 def menu_holerite():
+
+    while True:
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+        option = input('''    -----MENU HOLERITES-----
+        [1]: Gerar Holerite
+        [2]: Gerar Holerites Pendentes
     
-    option = input('''    -----MENU-----
-    [1]: Gerar Holerite
-    [2]: Gerar Holerites Pendentes
-
-    [9]: Voltar ao Menu Inicial
-    [0]: Sair do sistema
-                   
-    Selecione uma opção:''')
-
-    print('\n')
-
-    return None
+        [9]: Voltar ao Menu Inicial
+        [0]: Sair do sistema
+                       
+        Selecione uma opção:''')
+    
+        print('\n')
+    
+        return None
 
 
 
@@ -98,10 +106,11 @@ def main():
     
     cadastro = CadastroFuncionario()
     
-    
     while True:
+
+        os.system('cls' if os.name == 'nt' else 'clear')
     
-        option = input('''Bem vindo ao Menu!
+        option = input('''Bem vindo ao Menu! Escolha uma das opções:
     [1]: Funcionarios
     [2]: Holerites
     [0]: Sair do sistema
@@ -115,7 +124,7 @@ def main():
         elif option == '2':
             menu_holerite()
         elif option == '0':
-            print('Valeu!')
+            print('Saindo!')
             exit()
         else:
             print('Opção invalida!')
